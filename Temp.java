1fba08d3541b1868e27db97d4a9ad9e9da0a05d8
Fahmidur Rahman <100029999+FahmidurRahmanArnob@users.noreply.github.com>
2022-02-19 19:55:17 +0600

    This is My Template.
    I use this template to solve problems on Codeforces & Hackerrank.

---
+++ Temp.java
@@ -0,0 +1,51 @@
+import java.sql.SQLOutput;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Temp {
+    public static void main(String[] args){
+        FastScaner fs = new FastScaner();
+        System.out.println("Hello World");
+    }
+    static final Random random = new Random();
+    //Taken From "Second Thread"
+    static void ruffleSort(int[] a){
+        int n = a.length;//shuffles, then sort;
+        for(int i=0; i<n; i++){
+            int oi = random.nextInt(n), temp = a[oi];
+            a[oi] = a[i]; a[i] = temp;
+        }
+    }
+    static class FastScaner{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+        public String next(){
+            while(!st.hasMoreTokens()){
+                try{
+                    st = new StringTokenizer(br.readLine());
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        public int[] readArray(int n){
+            int[] a = new int[n];
+            for(int i=0; i<n; i++)a[i] = nextInt();
+            return a;
+        }
+        public int nextInt(){
+            return Integer.parseInt(next());
+        }
+        public long nextLong(){
+            return Long.parseLong(next());
+        }
+        public double nextDouble(){
+            return Double.parseDouble(next());
+        }
+        public float nextFloat(){
+            return Float.parseFloat(next());
+        }
+    }
+}
